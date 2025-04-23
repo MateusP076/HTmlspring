fc405016059056b77844855e0ce81b5c6e70b5f7
aluno cmc <alunocmc@umc.br>
2025-04-23 10:43:46 -0300

    primeiro

---
+++ src/main/java/coisas/example/CoisasApplication.java
@@ -0,0 +1,13 @@
+package coisas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoisasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coisas/example/Coisas/CoisasApplicationTests.java
@@ -0,0 +1,13 @@
+package coisas.example.Coisas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoisasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
