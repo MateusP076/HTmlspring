863523dc924dd83c847b7e7f80422154c1d11d70
aluno cmc <alunocmc@umc.br>
2025-04-25 10:56:21 -0300

    atualizando

--- src/main/java/coisas/example/coisinhas/CoisoController.java
+++ src/main/java/coisas/example/coisinhas/CoisoController.java
@@ -3,6 +3,7 @@ package coisas.example.coisinhas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
@@ -13,4 +14,15 @@ public class CoisoController {
                 "Mensagem motivacional=D");
         return "Teste";
     }
+
+    @PostMapping("/create")
+    public String create(Coisa coisa, Model model) {
+        System.out.println("Pessoa nome" +coisa.nome);
+        System.out.println("Email" + coisa.email);
+        System.out.println("Usuario" + coisa.usuario);
+        model.addAttribute("coisa", coisa);
+        return "candidate/info";
+    }
+    record Coisa(String nome, String email, String usuario) {}
 }
+
